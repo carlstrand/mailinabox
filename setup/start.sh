ae8d4b53729ddfa8eae467484ebf4ab7d6ce1ce7
carlstrand <43237772+carlstrand@users.noreply.github.com>
2019-02-08 10:20:48 +0100

    Update start.sh

--- setup/start.sh
+++ setup/start.sh
@@ -14,15 +14,15 @@ source setup/preflight.sh
 # Python may not be able to read/write files. This is also
 # in the management daemon startup script and the cron script.
 
-if ! locale -a | grep en_US.utf8 > /dev/null; then
+if ! locale -a | grep sv_SE.utf8 > /dev/null; then
     # Generate locale if not exists
-    hide_output locale-gen en_US.UTF-8
+    hide_output locale-gen sv_SE.UTF-8
 fi
 
-export LANGUAGE=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-export LC_TYPE=en_US.UTF-8
+export LANGUAGE=sv_SE.UTF-8
+export LC_ALL=sv_SE.UTF-8
+export LANG=sv_SE.UTF-8
+export LC_TYPE=sv_SE.UTF-8
 
 # Fix so line drawing characters are shown correctly in Putty on Windows. See #744.
 export NCURSES_NO_UTF8_ACS=1
