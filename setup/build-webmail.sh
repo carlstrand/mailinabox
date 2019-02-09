d2813d7c13f9a38217e7c8558fb48e23eaaec212
carlstrand <43237772+carlstrand@users.noreply.github.com>
2019-02-09 15:39:17 +0100

    Create build-webmail.sh

---
+++ setup/build-webmail.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+workdir_src="/tmp/rcube-src-git"
+
+git clone https://github.com/roundcube/roundcubemail.git ${workdir_src}
+git checkout master
+
+${workdir_src}/bin/install-jsdeps.sh
+${workdir_src}/bin/jsshrink.sh
+${workdir_src}/bin/updatecss.sh
+${workdir_src}/bin/cssshrink.sh
+
+(cd "${workdir_src}" && curl -sS https://getcomposer.org/installer | php -- --install-dir=/tmp/)
+(cd "${workdir_src}" && mv composer.json-dist composer.json && php composer.phar install --prefer-dist --no-dev)
+
+rm -rf tests plugins/*/tests .git* .tx* index-test.php Dockerfile vendor/pear/*/tests vendor/*/*/.git* vendor/pear/crypt_gpg/tools vendor/pear/console_commandline/docs vendor/pear/mail_mime/scripts vendor/pear/net_ldap2/doc vendor/pear/net_sm$
+
+tar czf /tmp/roundcubemail-release-complete.tar.gz rcube-src-git
