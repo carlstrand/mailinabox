c878a4b5127941c73eae60e3bc9d39b3470413d4
carlstrand <43237772+carlstrand@users.noreply.github.com>
2019-02-09 16:33:04 +0100

    Update packages-install.sh

--- setup/packages-install.sh
+++ setup/packages-install.sh
@@ -10,11 +10,11 @@ apt upgrade
 #sudo apt-fast dist-upgrade
 #sudo apt-fast remove
 
-apt-fast install aria2 bc bind9 bind9-host build-essential ca-certificates certbot coreutils cron curl dbconfig-common default-jre dialog dns \
+apt-fast install aria2 bc bind9 bind9-host build-essential ca-certificates certbot coreutils cron curl dbconfig-common default-jre \
 dovecot-antispam dovecot-core dovecot-imapd dovecot-lmtpd dovecot-managesieved dovecot-pop3d dovecot-sieve dovecot-sqlite duplicity fail2ban gcc git \
 google-perftools haveged ldnsutils libawl-php libcgi-fast-perl libgd-dev libgeoip-dev libgeoip1 libgoogle-perftools-dev libjs-jquery libjs-jquery-mousewheel \
 libmagic1 libmail-dkim-perl libpcre3 libpcre3-dev libperl-dev libssl-dev libxml2-dev libxslt-dev locales munin munin-node netcat-openbsd nsd ntp opendkim-tools opendmarc \
-openssh-client openssl pendkim php php-apcu php-cli php-common php-curl php-dev php-fpm php-gd php-imap php-intl php-json php-mbstring php-pear php-pspell php-soap php-sqlite3 \
+openssh-client openssl opendkim php php-apcu php-cli php-common php-curl php-dev php-fpm php-gd php-imap php-intl php-json php-mbstring php-pear php-pspell php-soap php-sqlite3 \
 php-xml php-xsl php-zip pollinate postfix postfix-pcre postfix-sqlite postgrey python-pip python3 python3-dev python3-pip pyzor razor rsyslog sed software-properties-common spampd \
 sqlite3 sudo tinymce ufw unattended-upgrades unzip uuid-dev virtualenv wget zlib1g zlib1g-dev zlibc
 
