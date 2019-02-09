93e1a5ef68ed80d548d70c55ab7856b9ca9f09c2
carlstrand <43237772+carlstrand@users.noreply.github.com>
2019-02-09 16:16:10 +0100

    Update packages-install.sh

--- setup/packages-install.sh
+++ setup/packages-install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-sudo apt-fast update
-sudo apt-fast remove -qq -y owncloud*
+#apt-fast update
+#apt-fast remove -qq -y owncloud*
 #apt-get purge -qq -y owncloud*
-sudo apt upgrade
+apt upgrade
 
 #sudo apt-fast update
 #sudo apt-fast install -y <package-name>
 #sudo apt-fast dist-upgrade
 #sudo apt-fast remove
 
-sudo apt-fast aria2 bc bind9 bind9-host build-essential ca-certificates certbot coreutils cron curl dbconfig-common default-jre dialog dns \
+apt-fast install aria2 bc bind9 bind9-host build-essential ca-certificates certbot coreutils cron curl dbconfig-common default-jre dialog dns \
 dovecot-antispam dovecot-core dovecot-imapd dovecot-lmtpd dovecot-managesieved dovecot-pop3d dovecot-sieve dovecot-sqlite duplicity fail2ban gcc git \
 google-perftools haveged ldnsutils libawl-php libcgi-fast-perl libgd-dev libgeoip-dev libgeoip1 libgoogle-perftools-dev libjs-jquery libjs-jquery-mousewheel \
 libmagic1 libmail-dkim-perl libpcre3 libpcre3-dev libperl-dev libssl-dev libxml2-dev libxslt-dev locales munin munin-node netcat-openbsd nsd ntp opendkim-tools opendmarc \
@@ -18,5 +18,4 @@ openssh-client openssl pendkim php php-apcu php-cli php-common php-curl php-dev 
 php-xml php-xsl php-zip pollinate postfix postfix-pcre postfix-sqlite postgrey python-pip python3 python3-dev python3-pip pyzor razor rsyslog sed software-properties-common spampd \
 sqlite3 sudo tinymce ufw unattended-upgrades unzip uuid-dev virtualenv wget zlib1g zlib1g-dev zlibc
 
-
-sudo apt autoremove
+apt autoremove
